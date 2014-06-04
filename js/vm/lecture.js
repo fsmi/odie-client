0d7281659ad8c84b477bb642f2e43b73b7bf2042
Max Wagner <max@trollbu.de>
2014-06-05 00:01:06 +0200

    Whitespace fix

--- js/vm/lecture.js
+++ js/vm/lecture.js
@@ -14,7 +14,7 @@ Lecture.prototype.load = function(name) {
   var self = this;
   this.name = name
   var urlName = encodeURIComponent(name);
-  $.getJSON(this.baseUrl + '/data/lectures/'+urlName+'/documents', function(data) {
+  $.getJSON(this.baseUrl + '/data/lectures/' + urlName + '/documents', function(data) {
     self.splice(0, self.length)
     for (var i=0; i<data.length; i++) {
       self.push(data[i])
