621e7b44a30b1025dd3676f43937dda77e4bd935
Sebastian Ullrich <sebasti@nullri.ch>
2014-07-04 12:52:52 +0200

    Actually implement 'Remember Me' checkbox

--- js/vm/user.js
+++ js/vm/user.js
@@ -3,6 +3,7 @@ var User = function(baseUrl) {
   this.baseUrl = baseUrl;
   this.username = '';
   this.password = '';
+  this.rememberMe = true;
   this.errorThrown = '';
   this.onAuthUpdate = function() {
     // This cookie is set when the login API call returns 200.
@@ -23,7 +24,7 @@ User.prototype.login = function(success) {
     url: this.baseUrl + '/data/login',
     type: 'POST',
     contentType: 'application/json; charset=UTF-8',
-    data: JSON.stringify({user: this.username, password: this.password}),
+    data: JSON.stringify({user: this.username, password: this.password, rememberMe: this.rememberMe}),
     error: function(xhr, _, errorThrown) {
       self.errorThrown = xhr.status + ': ' + xhr.responseText;
       self.onAuthUpdate();
