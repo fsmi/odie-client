67f33a2fc3bbdc03ca6c68e44308ee4fc0f759af
sycoso <sycoso@sycoso.eu>
2014-10-02 13:10:47 +0200

    Add function to search for examinants (regex) in all exams
    WARNING: takes quite a while since it's a client-side hack

--- js/vm/documentlist.js
+++ js/vm/documentlist.js
@@ -2,12 +2,16 @@ var DocumentList = function(baseUrl) {
   var self = this;
   this.baseUrl = baseUrl;
   this.lecture = '';
+  this.examinants = '';
   this.lectureFilter = '';
   this.examinantsFilter = '';
   this.typeFilter = '';
   ko.track(this);
   ko.getObservable(this, 'lecture').subscribe(function(newName) {
     self.load(newName);
+  })
+  ko.getObservable(this, 'examinants').subscribe(function(newExaminants) {
+    self.loadExaminants(newExaminants);
   })
 }
 
@@ -21,6 +25,34 @@ DocumentList.prototype.load = function(name) {
     self.splice(0, self.length);
     for (var i=0; i<data.length; i++) {
       self.push(new Document(data[i]));
+    }
+  })
+}
+
+DocumentList.prototype.loadExaminants = function(newExaminants) {
+  var self = this;
+  if (newExaminants == '') {
+    return;
+  }
+  var examinantsRegex = new RegExp(newExaminants, 'i');
+  if (examinantsRegex === '') {
+    return;
+  }
+  self.splice(0, self.length);
+  // get list of lectures
+  $.getJSON(self.baseUrl + '/data/lectures', function(lectures) {
+    for (var i=0; i<lectures.length; i++) {
+      var urlName = encodeURIComponent(lectures[i]['name']);
+      // get list of exams
+      $.getJSON(self.baseUrl + '/data/lectures/' + urlName + '/documents', function(exams) {
+        for (var j=0; j<exams.length; j++) {
+          var document = new Document(exams[j]);
+          // filter for examinants
+          if (examinantsRegex.test(document.examinants.join(' '))) {
+            self.push(document);
+          }
+        }
+      })
     }
   })
 }
