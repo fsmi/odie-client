33ca78a51479822c091407943f4637d43019331a
Sebastian Ullrich <sebasti@nullri.ch>
2015-02-20 18:29:11 +0100

    Switch from broccoli-static-compiler to broccoli-funnel
    "I'm big +1 on literally any other package than broccoli-static-compiler, maintained by literally anyone other than me ;-) Yay for funnel!"

--- Brocfile.js
+++ Brocfile.js
@@ -1,19 +1,15 @@
 var concat = require('broccoli-concat');
 var mergeTrees = require('broccoli-merge-trees');
-var pickFiles = require('broccoli-static-compiler');
+var Funnel = require('broccoli-funnel');
 var esTranspiler = require('broccoli-babel-transpiler');
 
 var js = 'js/';
-var es6js = pickFiles(js, {
-  srcDir: '/',
-  destDir: '/',
-  files: ['app.js', 'lib.js', 'ko/*.js', 'vm/*.js']
+var es6js = new Funnel(js, {
+  include: ['app.js', 'lib.js', 'ko/*.js', 'vm/*.js']
 });
 es6js = esTranspiler(es6js);
-var es6polyfill = pickFiles('node_modules/broccoli-babel-transpiler/node_modules/babel-core', {
-  srcDir: '/',
-  destDir: '/',
-  files: ['browser-polyfill.js']
+var es6polyfill = new Funnel('node_modules/broccoli-babel-transpiler/node_modules/babel-core', {
+  include: ['browser-polyfill.js']
 });
 js = mergeTrees([js, es6polyfill, es6js], { overwrite: true });
 js = concat(js, {
@@ -30,10 +26,9 @@ css = concat(css, {
 });
 
 var fonts = 'fonts/';
-fonts = pickFiles(fonts, {
-  srcDir: '/',
-  destDir: '/fonts',
-  files: ['*.woff']
+fonts = new Funnel(fonts, {
+  include: ['*.woff'],
+  destDir: '/fonts'
 });
 
 var views = 'views/';
