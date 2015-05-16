b34507698f6014d72120d4555ac760254f615583
sycoso <sycoso@sycoso.eu>
2015-05-16 16:31:17 +0200

    Merge typeaheadlist into documentselection

--- js/vm/documentselection.js
+++ js/vm/documentselection.js
@@ -5,15 +5,14 @@ import config from "../config";
 import user from "./user";
 import Cart from "./cart";
 import DocumentList from "./documentlist";
-import TypeaheadList from "./typeaheadlist";
 import RangeSelect from "./rangeselect";
 
 class DocumentSelection {
   constructor() {
     this.cart = new Cart();
     this.documentlist = new DocumentList();
-    this.lecturelist = new TypeaheadList('lectures');
-    this.examinantlist = new TypeaheadList('examinants');
+    config.getJSON('/data/lectures').done(data => this.lectureTypeaheadList = data);
+    config.getJSON('/data/examinants').done(data => this.examinantTypeaheadList = data);
     this.rangeSelect = new RangeSelect(this.cart);
     this.searchBy = 'lecture';
 
@@ -30,12 +29,25 @@ class DocumentSelection {
 
   get typeaheadDataset() {
     return {
-      source: (query, callback) => (this.searchBy == 'lecture' ? this.lecturelist : this.examinantlist).typeaheadDataset.source(query, callback),
+      source: (query, callback) => {
+        let regex = this.getSearchRegex(query);
+        callback((this.searchBy == 'lecture' ? this.lectureTypeaheadList : this.examinantTypeaheadList).filter(l => regex.test(l.name)));
+      },
       displayKey: "name",
       templates: {
         suggestion: l => `<a href="#">${l.name}</a>`
       }
     };
+  }
+
+  getSearchRegex(searchString) {
+    if (/^([A-Z][a-z]*){2,}$/.test(searchString)) {
+      // PascalCase search for search strings with more than one capital char
+      return new RegExp(searchString.split(/(?=[A-Z])/).join('[^A-Z]*'));
+    } else {
+      // standard case-insensitive search
+      return new RegExp(searchString, 'i');
+    }
   }
 }
 
