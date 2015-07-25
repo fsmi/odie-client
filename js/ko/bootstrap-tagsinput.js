d058a63702990303d31e9047d0d2e7b7ce4f9489
Sebastian Ullrich <sebasti@nullri.ch>
2015-07-25 14:54:40 +0200

    tagsinput: two-way binding

--- js/ko/bootstrap-tagsinput.js
+++ js/ko/bootstrap-tagsinput.js
@@ -1,16 +1,30 @@
 import $ from "jquery";
 import ko from "knockout";
+import all from "lodash/collection/all";
+import zip from "lodash/array/zip";
 import "bootstrap-tagsinput/src/bootstrap-tagsinput";
 
 ko.bindingHandlers.tagsinput = {
   init(element, valueAccessor) {
+    function pull() {
+      for (let item of o.items())
+        e.tagsinput('add', item);
+    }
+    function sameItems(xs, ys) {
+      return xs.length === ys.length && all(zip(xs, ys).map(([x, y]) => x.obj === y.obj));
+    }
+
     let e = $(element);
     let o = ko.unwrap(valueAccessor());
+    o.items.subscribe(() => {
+      if (!sameItems(e.tagsinput('items'), o.items())) {
+        e.tagsinput('removeAll');
+        pull();
+      }
+    });
 
     e.tagsinput(o);
-    for (let item of o.items())
-      e.tagsinput('add', item);
-
+    pull();
     e.on('itemAdded itemRemoved', () => o.items(e.tagsinput('items')));
   },
 };
