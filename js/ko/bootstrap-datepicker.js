2315b381f2adb27c5c67cd49997a39796f00d004
jspam <jspam@users.noreply.github.com>
2015-09-26 12:03:44 +0200

    Localize the document submission datepicker
    The locale was set as a parameter for the datepicker in the view, but
    the corresponding JS file was not included.

--- js/ko/bootstrap-datepicker.js
+++ js/ko/bootstrap-datepicker.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import ko from "knockout";
 
 import "bootstrap-datepicker/js/bootstrap-datepicker";
+import "bootstrap-datepicker/js/locales/bootstrap-datepicker.de";
 
 ko.bindingHandlers.datepicker = {
   init(element, valueAccessor) {
