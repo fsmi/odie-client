fe7d3eb07e39c6bcfe0805ee1268f7b0bc35944c
fjalir <christoph.rohmann@googlemail.com>
2017-05-26 20:42:16 +0200

    everything except log implemented for document rejection

--- js/vm/depositreturn.js
+++ js/vm/depositreturn.js
@@ -14,7 +14,7 @@ export default class DepositReturn {
     this.documentFilter = new SubstringFilter({column: 'submitted_by'});
     this.depositFilter = new SubstringFilter({column: 'name'});
 
-    this.validatedIDs = []; //window.localStorage && JSON.parse(localStorage.getItem("Odie_ValidatedDocuments")) || [];
+    this.locallyValidatedIDs = []; //window.localStorage && JSON.parse(localStorage.getItem("Odie_ValidatedDocuments")) || [];
     this.documents = new SelectableCollection({
       endpoint: 'documents',
       filters: [
@@ -24,7 +24,7 @@ export default class DepositReturn {
       ],
       sortBy: {column: 'date', asc: false},
       deserialize: data => {
-        if(this.validatedIDs.indexOf(data.id) !== -1) data.validated = true;
+        if(this.locallyValidatedIDs.indexOf(data.id) !== -1) data.validated = true;
         return new Document(data);
       },
     });
@@ -41,9 +41,19 @@ export default class DepositReturn {
 
   validate(document) {
     window.open(document.previewURL, '_blank');
-    document.validated = true;
-    if(this.validatedIDs.indexOf(document.id) === -1) this.validatedIDs.push(document.id);
-    //if(window.localStorage) window.localStorage.setItem("Odie_ValidatedDocuments",JSON.stringify(this.validatedIDs));
+    if(!document.validated) {
+      document.validated = true;
+      if(this.locallyValidatedIDs.indexOf(document.id) === -1) this.locallyValidatedIDs.push(document.id);
+    }
+    //if(window.localStorage) window.localStorage.setItem("Odie_ValidatedDocuments",JSON.stringify(this.locallyValidatedIDs));
+  }
+
+  currentSelectionEligibleForRejection() {
+    // lets not fail the servers delete_document pre-condition
+    let selected = this.documents.selected;
+    return selected &&
+        (selected.early_document_eligible || selected.deposit_return_eligible) && // be still eligible for smth
+        (!selected.validated || this.locallyValidatedIDs.indexOf(selected.id) === 1); // either locally validated or not at all
   }
 
   cashOutDeposit() {
@@ -70,6 +80,12 @@ export default class DepositReturn {
 
     api.post('log_early_document_reward', data).done((data) => {
       log.addItem('Erstprotokoll', -data.data.disbursal);
+      this.documents.load();
+    });
+  }
+
+  rejectDocument() {
+    api.delete(`documents/${this.documents.selected.id}`).done((data) => {
       this.documents.load();
     });
   }
